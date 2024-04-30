848b927f2acebf30540eaabeaa8b333cd6be44f2
ZC <peng05831@163.com>
2024-04-30 15:26:46 +0800

    docs(other): 配置注释补充

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
+// 配置文件导出对象
 module.exports = {
+  // 定义代码运行的环境
   env: {
-    browser: true,
-    es2021: true,
-    node: true
+    browser: true, // 浏览器环境
+    es2021: true, // ES2021标准
+    node: true // Node.js环境
   },
   extends: [
-    // 'airbnb-base',
-    'plugin:prettier/recommended' // 添加 prettier 插件
+    // 'airbnb-base', // 扩展自airbnb-base的规则，此处已注释
+    'plugin:prettier/recommended' // 扩展自prettier的推荐规则
   ],
+  // 解析器设置
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 'latest', // 支持最新的ECMAScript标准
+    sourceType: 'module' // 模块源类型设置为ES模块
   },
+  // 使用的插件
   plugins: ['@typescript-eslint'],
+  // 自定义的规则
   rules: {
-    'import/no-unresolved': 'off',
-    'import/extensions': 'off'
+    'import/no-unresolved': 'off', // 关闭import路径未解析的警告
+    'import/extensions': 'off' // 关闭对import路径后缀的警告
   }
 }
